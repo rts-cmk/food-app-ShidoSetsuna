1bea6733b2bfd396d77c1da0c8ba623e38bf7214
Valdemar Larsen <shido813@gmail.com>
2025-10-07 11:13:00 +0200

    Final clean up, fixed filter icon

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,5 +1,6 @@
 import "./search.css";
 import { useState } from "react";
+import filterIcon from "../../assets/filter.svg";
 
 export default function Search({ onSearch }) {
   const [searchQuery, setSearchQuery] = useState("");
@@ -33,7 +34,7 @@ export default function Search({ onSearch }) {
             <path
               d="M21 21L16.514 16.506L21 21ZM19 10.5C19 15.194 15.194 19 10.5 19C5.806 19 2 15.194 2 10.5C2 5.806 5.806 2 10.5 2C15.194 2 19 5.806 19 10.5Z"
               stroke="currentColor"
-              strokeWidth="2"
+              strokeWidth="4"
               strokeLinecap="round"
               strokeLinejoin="round"
             />
@@ -41,6 +42,7 @@ export default function Search({ onSearch }) {
         </button>
         <input
           type="text"
+          name="search"
           placeholder="Search"
           className="search-input"
           value={searchQuery}
@@ -49,20 +51,7 @@ export default function Search({ onSearch }) {
       </form>
 
       <button className="filter-button">
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M3 7H21L3 7ZM6 12H18H6ZM10 17H14H10Z"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        <img src={filterIcon} alt="Filter" className="filter-icon" />
       </button>
     </div>
   );
